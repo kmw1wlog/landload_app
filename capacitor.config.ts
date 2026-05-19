b66298b5803416a394baf4ebec99c69eed052170
MINWOOK KIM <kmw1wlog@unist.ac.kr>
2026-05-19 12:57:25 +0900

    Add Capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.kmw1wlog.landloadapp",
+  appName: "landload_app",
+  webDir: "out"
+};
+
+export default config;
